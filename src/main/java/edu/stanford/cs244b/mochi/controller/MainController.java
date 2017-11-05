24f9a3158f0cb4ed761b4b69561ad08e2539e2ab
Tigran <tigran.tsat@gmail.com>
2017-11-04 18:06:57 -0700

    SLF4J Logging

--- src/main/java/edu/stanford/cs244b/mochi/controller/MainController.java
+++ src/main/java/edu/stanford/cs244b/mochi/controller/MainController.java
@@ -1,13 +1,17 @@
 package edu.stanford.cs244b.mochi.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+    final static Logger LOG = LoggerFactory.getLogger(MainController.class);
 
     @RequestMapping("/")
     public String index() {
+        LOG.info("Inside Main Controller");
         return "Greetings from Spring Boot!";
     }
 }
